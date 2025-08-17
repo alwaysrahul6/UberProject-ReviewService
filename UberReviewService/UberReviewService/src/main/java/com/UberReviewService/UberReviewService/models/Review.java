da19d1542c0cf002c7c5133733ee6fd68854d6d0
Sharma Rahul <115923375+alwaysrahul6@users.noreply.github.com>
2025-08-17 19:39:37 +0530

    Added more Coloumns for review table

--- UberReviewService/UberReviewService/src/main/java/com/UberReviewService/UberReviewService/models/Review.java
+++ UberReviewService/UberReviewService/src/main/java/com/UberReviewService/UberReviewService/models/Review.java
@@ -1,12 +1,33 @@
 package com.UberReviewService.UberReviewService.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import java.util.Date;
+
 
 @Entity
+@Table(name = "bookingReview")
 public class Review {
     @Id /// this annotion make the id propert is a primrey key of our table
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     Long id;
+    @Column(nullable = false)
+    String content;
 
+    Double rating;
 
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreatedDate
+    Date createdAt;
+
+    @Column(nullable = false)
+    @Temporal(TemporalType.TIMESTAMP)
+    @LastModifiedDate
+    Date updatedAt;
 }
